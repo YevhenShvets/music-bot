7efd84790f4c5307e5b513ef9cd9f7b5c3b22f8b
YevhenShvets <shvets_06@ukr.net>
2020-12-15 21:10:27 +0200

    Add start commant

--- src/commands/help.py
+++ src/commands/help.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from src.core import dp
+from pathlib import Path
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
@@ -11,3 +12,21 @@ async def send_help(message: types.Message):
     This handler will be called when user sends `/help` command
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+
+
+@dp.message_handler(commands='start')
+async def start_command(message: types.Message):
+    """
+    This handler will be called when user sends `/start` command
+    """
+    BASE_DIR = Path(__file__).resolve().parent.parent.parent
+    BASE_DIR = BASE_DIR.joinpath('data/img/')
+
+    # await message.answer("Hello " + message.from_user.full_name +"   "+ BASE_DIR.joinpath(
+    # 'data/img/img1.png').__str__())
+    reply_keyboard =ReplyKeyboardMarkup(resize_keyboard=True)
+    but1 = KeyboardButton("Hello!!!!")
+    but2 = KeyboardButton("Hi!")
+    reply_keyboard.add(but1)
+    reply_keyboard.add(but2)
+    await message.answer_photo(open(BASE_DIR.joinpath('img1.png').__str__(), 'rb'), reply_markup=reply_keyboard)
